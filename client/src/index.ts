cc39324774fae776e36ba92e8fea893c2cf3a90c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-06 08:39:56 +0900

    WebPackの実験

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,9 @@
-console.info("Hello");
+import msgpack from "msgpack5";
+
+const packer = msgpack();
+
+const encoded = packer.encode({name: "hoge", age: 15});
+console.info("encoded", encoded);
+
+const decoded = packer.decode(encoded);
+console.info("decoded", decoded);
